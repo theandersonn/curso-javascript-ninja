79c8970243f7d65b092576a83098259a05975ec1
theandersonn <theandersonn@gmail.com>
2020-09-07 15:55:32 -0300

    Add aula 08 - parte2

--- aula-08/desafio.js
+++ aula-08/desafio.js
@@ -106,22 +106,28 @@ function calculator(operator) {
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-// ?
+var sum = calculator("+");
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-// ?
+sum(7, 8);
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+var subtraction = calculator("-");
+var multiplication = calculator("*");
+var division = calculator("/");
+var mod = calculator("%");
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-// ?
+subtraction(10, 2);
+multiplication(25, 5);
+division(3, 2);
+mod(6, 3);
